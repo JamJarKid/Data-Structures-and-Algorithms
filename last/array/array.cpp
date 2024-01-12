1a1e0916309c1adf8755fcc71e315b893a45e4a4
malijah <41331314+Malijah-C@users.noreply.github.com>
2024-01-12 17:10:16 +0000

    made stack allocated array in c++ (just practice)

--- last/array/array.cpp
+++ last/array/array.cpp
@@ -1,1 +1,31 @@
+#include <initializer_list>
 #include <iostream>
+
+template <typename T, size_t S> class Array {
+public:
+  Array(std::initializer_list<T> init) {
+    size_t i = 0;
+    for (auto &value : init) {
+      m_data[i] = value;
+      ++i;
+    }
+  }
+
+  Array() {}
+
+  constexpr size_t Size() const { return S; }
+
+  T &operator[](size_t index) { return m_data[index]; }
+  const T &operator[](size_t index) const { return m_data[index]; }
+
+private:
+  T m_data[S] = {};
+};
+
+int main() {
+  Array<int, 5> data{1, 10, 100, 1000, 5000};
+
+  for (size_t i = 0; i < data.Size(); ++i) {
+    std::cout << data[i] << std::endl;
+  }
+}
